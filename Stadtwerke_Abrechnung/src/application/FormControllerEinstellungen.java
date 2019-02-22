81a0013198889c7ff3e842fc9cd9aec20204a201
D070550 <D070550@WDFN34222702A.emea.global.corp.sap>
2019-02-22 16:41:48 +0100

    Neue Einstellungen hinzufügen - Layou t0,5h

--- Stadtwerke_Abrechnung/src/application/FormControllerEinstellungen.java
+++ Stadtwerke_Abrechnung/src/application/FormControllerEinstellungen.java
@@ -4,6 +4,8 @@ package application;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.jfoenix.controls.JFXTextField;
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -22,6 +24,18 @@ public class FormControllerEinstellungen {
 	private Label faktor_wasser;
 	@FXML
 	private Label faktor_abwasser;
+	
+	//Neue Einstellungen:
+	@FXML
+	private ComboBox<Integer> cb_einstellung_neu_id;
+	@FXML
+	private JFXTextField tf_faktor_strom;
+	@FXML
+	private JFXTextField tf_faktor_erdgas;
+	@FXML
+	private JFXTextField tf_faktor_wasser;
+	@FXML
+	private JFXTextField tf_faktor_abwasser;
 	
 	
 	public void initialize() {
@@ -45,6 +59,7 @@ public class FormControllerEinstellungen {
 			}
 
 			cb_einstellung_id.setItems(list_einstellungen_ids);
+			cb_einstellung_neu_id.setItems(list_einstellungen_ids);
 
 		} catch (SQLException e) {}
 	}
@@ -64,6 +79,27 @@ public class FormControllerEinstellungen {
 				faktor_erdgas.setText(rs.getString("faktor_erdgas"));
 				faktor_wasser.setText(rs.getString("faktor_wasser"));
 				faktor_abwasser.setText(rs.getString("faktor_abwasser"));
+
+			}
+
+
+		} catch (SQLException e) {}
+	}
+	
+	public void action_einstellung_neu_id_gewaehlt() {
+		System.out.println(cb_einstellung_id.getValue());
+		
+		DB db = new DB();
+		ResultSet rs = db.executeQueryWithResult("SELECT * FROM `einstellung` WHERE `id` = "+cb_einstellung_id.getValue()+"");
+
+		try {
+			
+			if (rs.next()) {
+
+				tf_faktor_strom.setText(rs.getString("faktor_strom"));
+				tf_faktor_erdgas.setText(rs.getString("faktor_erdgas"));
+				tf_faktor_wasser.setText(rs.getString("faktor_wasser"));
+				tf_faktor_abwasser.setText(rs.getString("faktor_abwasser"));
 
 			}
 
