b34b122f315669d581c33efdffb430aa2c06e41e
D070550 <D070550@WDFN34222702A.emea.global.corp.sap>
2019-03-07 21:16:29 +0100

    Entwurf + Umsetzung Seite Aktueller Stand 0,5

--- Stadtwerke_Abrechnung/src/application/FormControllerAktuellerStand.java
+++ Stadtwerke_Abrechnung/src/application/FormControllerAktuellerStand.java
@@ -1,7 +1,81 @@
 package application;
 
+import java.sql.ResultSet;
+
+import com.jfoenix.controls.JFXTextField;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class FormControllerAktuellerStand {
 	
+	@FXML
+	private JFXTextField tf_zaehler_strom;
+	@FXML
+	private JFXTextField tf_zaehler_erdgas;
+	@FXML
+	private JFXTextField tf_zaehler_wasser;
+	@FXML
+	private JFXTextField tf_zaehler_abwasser;
+	@FXML
+	private Label lb_menge_strom;
+	@FXML
+	private Label lb_menge_erdgas;
+	@FXML
+	private Label lb_menge_wasser;
+	@FXML
+	private Label lb_menge_abwasser;
+	@FXML
+	private Label lb_vergleichswert_strom;
+	@FXML
+	private Label lb_vergleichswert_erdgas;
+	@FXML
+	private Label lb_vergleichswert_wasser;
+	@FXML
+	private Label lb_vergleichswert_abwasser;
+	@FXML
+	private Label lb_abweichung_strom;
+	@FXML
+	private Label lb_abweichung_erdgas;
+	@FXML
+	private Label lb_abweichung_wasser;
+	@FXML
+	private Label lb_abweichung_abwasser;
+	
+	@FXML
+	private Button bt_berechnen;
+	
+	
+	public void action_button_berechnen() {
+		
+		//Eingabewerte speichern
+		int zaehler_strom = Integer.parseInt(tf_zaehler_strom.getText());
+		int zaehler_erdgas = Integer.parseInt(tf_zaehler_erdgas.getText());
+		int zaehler_wasser = Integer.parseInt(tf_zaehler_wasser.getText());
+		int zaehler_abwasser = Integer.parseInt(tf_zaehler_abwasser.getText());
+		
+		//Zeitraum finden
+		DB db = new DB();
+		String sql_zeitraum = "SELECT `id`, `zeitraum_von`, `zeitraum_bis` FROM `zeitraum` ORDER BY `zeitraum_bis` DESC LIMIT 1";
+
+		ResultSet rs_zeitraum = db.executeQueryWithResult(sql_zeitraum);
+
+		try {
+			if (rs_zeitraum.next()) {
+				
+			
+				int id = rs_zeitraum.getInt("id");
+				java.sql.Date sql_date_zeitraum_von = rs_zeitraum.getDate("zeitraum_von");
+				java.sql.Date sql_date_zeitraum_bis = rs_zeitraum.getDate("zeitraum_bis");
+				System.out.println(""+id);
+				System.out.println(sql_date_zeitraum_von);
+				System.out.println(sql_date_zeitraum_bis);
+				
+			}	
+		}catch(Exception e) {}
+		
+	}
 	
 	public void action_menu_uebersicht() {
 		Main.setStage("Uebersicht");
